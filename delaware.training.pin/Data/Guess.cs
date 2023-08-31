a1d20f2d08d12b6f66efd9cf339cb8bffdf0f794
Wouter Hemeryck <wouter.hemeryck@delawareconsulting.com>
2023-08-31 12:42:49 +0200

    Initial commit with basic guessing

---
+++ delaware.training.pin/Data/Guess.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace delaware.training.pin.Data
+{
+    public class Guess
+	{
+		[Required]
+		[MaxLength(4)]
+		[Range(0,9999)]
+		public int? Value { get; set; }
+	}
+}
+
---
+++ delaware.training.pin/Data/GuessService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
+namespace delaware.training.pin.Data
+{
+	public class GuessService
+	{
+		private int _pincode;
+
+        public ConcurrentDictionary<int, int> Guesses { get; private set; } = new();
+
+        public bool IsGuessed { get; private set; }
+
+
+        public GuessService()
+		{
+			SetRandomPin();
+		}
+
+		public void SetRandomPin()
+		{
+			var random = new Random();
+			_pincode = random.Next(0, 9999);
+			IsGuessed = false;
+        }
+
+        public bool GuessPin(int guess)
+        {
+            Guesses.AddOrUpdate(guess, 1, (key, oldValue) => oldValue + 1);
+
+			if (guess == _pincode)
+			{
+				IsGuessed = true;
+				return true;
+			}
+
+			return false;
+        }
+    }
+}
+
